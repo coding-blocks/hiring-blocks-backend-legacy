21533c39b957e3d8c411d909201228e5c2289fb7
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-02-12 12:49:50 +0530

    Update Jobs resource to include more required attributes
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- dist/models/jobs.js
+++ dist/models/jobs.js
@@ -15,12 +15,20 @@ jsonAPI.define({
     attributes: {
         id: Joi.string(),
         title: Joi.string().required(),
+        skills: Joi.array().items(Joi.string()).required(),
+        jobType: Joi.string().required(),
+        startDate: Joi.date().required(),
+        deadline: Joi.date().required(),
         company: Joi.one('companies').uidType("autoincrement").required()
     },
     examples: [
         {
             type: 'jobs',
             title: 'Teaching Assistant',
+            skills: ['C++'],
+            jobType: 'Internship',
+            startDate: new Date('2018-02-04'),
+            deadline: new Date('2018-04-15'),
             company: {
                 id: '1',
                 type: 'companies',
